25e1eda30ff10c1e6ca10ab98ff832708a27b346
Alexey Hohlov <alexey.hohl0v@yandex.ru>
2023-07-05 03:37:20 +0700

    feat: tilt for photo and info; some new animations

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -4,6 +4,7 @@ import { Bullet } from '../../components';
 import { motion } from 'framer-motion';
 import { aboutBullets } from '../../data/listItems';
 import { scale, slideFromBottom, slideFromLeft } from '../../utils/animations';
+import Tilt from 'react-parallax-tilt';
 
 const About: React.FC = () => {
     const motionProps = {
@@ -20,22 +21,30 @@ const About: React.FC = () => {
     return (
         <motion.section className={styles.about} id={'about'} {...motionProps}>
             <div className={styles.content}>
-                <div className={styles.info}>
-                    <motion.img src={photo} alt='' variants={slideFromLeft} />
-                    <div className={styles.text}>
-                        <motion.p custom={1} variants={scale}>
-                            Я занимаюсь front-end разработкой. Создаю плавные,
-                            интуитивные, адаптивные веб-приложения и интерфейсы
-                        </motion.p>
-                        <motion.p custom={2} variants={scale}>
-                            Связаться со мной, а так же ознакомиться с моими
-                            навыками можно ниже.
-                        </motion.p>
+                <Tilt tiltMaxAngleX={2} tiltMaxAngleY={2} reset={false}>
+                    <div className={styles.info}>
+                        <motion.img
+                            src={photo}
+                            alt=''
+                            variants={slideFromLeft}
+                        />
+                        <div className={styles.text}>
+                            <motion.p custom={1} variants={scale}>
+                                Я занимаюсь front-end разработкой. Создаю
+                                плавные, интуитивные, адаптивные веб-приложения
+                                и интерфейсы
+                            </motion.p>
+                            <motion.p custom={2} variants={scale}>
+                                Связаться со мной, а так же ознакомиться с моими
+                                навыками можно ниже.
+                            </motion.p>
+                        </div>
                     </div>
-                </div>
+                </Tilt>
                 <motion.ul className={styles.bullets} {...motionProps}>
                     {aboutBullets.map((bullet, index) => (
                         <MBullet
+                            whileHover={{ scale: 1.15 }}
                             variants={slideFromBottom}
                             custom={index + 1}
                             key={bullet.header}
